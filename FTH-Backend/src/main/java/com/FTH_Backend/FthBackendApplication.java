d30e7aac1beef2741fa2099f2eb2fd0e20e64790
Katta Harsha Vardhan <109226763+KHarshaVardhanC@users.noreply.github.com>
2025-03-24 16:34:58 +0530

    Add files via upload

---
+++ FTH-Backend/src/main/java/com/FTH_Backend/FthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.FTH_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FthBackendApplication.class, args);
+	}
+
+}
