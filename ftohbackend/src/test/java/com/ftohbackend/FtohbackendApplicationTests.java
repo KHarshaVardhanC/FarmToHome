01055bc54d5f8657fbe5d51d9d117c0bdd4b22ab
Katta Harsha Vardhan <109226763+KHarshaVardhanC@users.noreply.github.com>
2025-03-25 12:38:25 +0530

    Add files via upload

---
+++ ftohbackend/src/test/java/com/ftohbackend/FtohbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftohbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtohbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
