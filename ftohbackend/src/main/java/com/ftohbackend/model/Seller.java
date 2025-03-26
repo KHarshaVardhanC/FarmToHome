b8bef172e4759cd2438bf4bc934ced021fe34468
katta <kattaharshavardhan56@gmail.com>
2025-03-26 14:43:01 +0530

    gitignoreupdate

--- ftohbackend/src/main/java/com/ftohbackend/model/Seller.java
+++ ftohbackend/src/main/java/com/ftohbackend/model/Seller.java
@@ -1,18 +1,32 @@
 package com.ftohbackend.model;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Table(name="Seller")
 @Entity
 public class Seller {
-
+  
 	
-	
+	@Id
+	@GeneratedValue()
 	Integer sellerId;
-	String sellerFirstName;
-	String sellerLastName;
-	String sellerPassword;
+	@Column(nullable = false)
 	String sellerEmail;
+	@Column(nullable  = false)
+	String sellerFirstName;
+	@Column(nullable = false)
+	String sellerLastName;
+	@Column(nullable = false)
+	Date sellerDOB;
+	@Column(nullable = false)
+	String sellerPassword;
+	@Column(nullable = false)
+	String sellerStatus;
 	
 }
