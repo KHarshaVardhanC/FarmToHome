8a8546c027341ce6b34d8ea2b7f0ccbf0d2142ba
MahmadFarha <mahmadfarha43@gmail.com>
2025-04-03 17:44:12 +0530

    customer45

--- ftohbackend/src/main/java/com/ftohbackend/model/AllOrders.java
+++ ftohbackend/src/main/java/com/ftohbackend/model/AllOrders.java
@@ -13,15 +13,15 @@ public class AllOrders {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderId;
 
-<<<<<<< HEAD
-  //  @ManyToOne
-   // @JoinColumn(name = "product_id", nullable = false)
-    //private AllProduct product;
-=======
+
+   @ManyToOne
+   @JoinColumn(name = "product_id", nullable = false)
+    private AllProduct product;
+
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
     private allProductModel product;
->>>>>>> bea7448b35aac8b86ebf9a3016de2bf4aa66a71b
+
 
     @ManyToOne
     @JoinColumn(name = "customer_id", nullable = false)
