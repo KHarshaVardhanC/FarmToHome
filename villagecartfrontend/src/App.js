d69c0011f5e01e25e2e722470df6cfb736f7a93b
MahmadFarha <mahmadfarha43@gmail.com>
2025-04-11 15:18:39 +0530

    login

--- villagecartfrontend/src/App.js
+++ villagecartfrontend/src/App.js
@@ -11,8 +11,8 @@ import ViewRatings from './pages/ViewRatings';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import MainHome from './pages/MainHome';
-// import SignIn from './pages/Login/SignIn';
-// import SignUp from './pages/Login/SignUp';
+import SignIn from './pages/Login/SignIn';
+import SignUp from './pages/Login/SignUp';
 
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -38,8 +38,8 @@ function App() {
         <Route path="/add-product" element={<AddProduct />} />
 
           {/* ✅ Auth Routes */}
-          {/* <Route path="/login" element={<SignIn />} />
-          <Route path="/register" element={<SignUp />} /> */}
+        <Route path="/signup" element={<SignUp />} /> 
+        <Route path="/login" element={<SignIn/>}/>
         </Routes>
       </div>
     </Router>
