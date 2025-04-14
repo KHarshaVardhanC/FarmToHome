50e156a1d0d3cf619bf2dcbd4ffaab0a47809b03
katta <kattaharshavardhan56@gmail.com>
2025-04-14 11:43:26 +0530

    somevalidation

--- ftohbackend/src/main/java/com/ftohbackend/dto/LoginRequest.java
+++ ftohbackend/src/main/java/com/ftohbackend/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.ftohbackend.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public  class LoginRequest {
+	
+	@NotNull(message = "Email cannot be null")
+	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
 	private String email;
+	
+	@NotNull(message = "Password cannot be null")
+//	@Size(min = 4, message = "Password must be at least 8 characters long")
 	private String password;
 
 }
--- ftohbackend/src/main/java/com/ftohbackend/dto/RatingDTO.java
+++ ftohbackend/src/main/java/com/ftohbackend/dto/RatingDTO.java
@@ -1,5 +1,6 @@
 package com.ftohbackend.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 public class RatingDTO {
 
 	private Integer ratingId;
+	@NotNull(message = "Customer Id is Required")
 	private Integer customerId;
+	@NotNull(message = "Product Id is Required")
 	private Integer productId;
+	@NotNull(message = "Rating Stars is Required")
 	Double ratingValue;
 	private String feedback;
 
